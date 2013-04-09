3c88cdab346c794b5d6a1bce92aab3569cc88fa2
Sarudak <SPaulson@paraport.com>
2013-04-09 12:35:07 -0700

    Made things a little prettier

--- bin/WebTest/handler.clj
+++ bin/WebTest/handler.clj
@@ -10,11 +10,11 @@
   (GET "/" [] (pages/index "" false))
   (GET "/Quickletters/:yearmonth" [yearmonth] (->> (data/getQuickLetters yearmonth)
 																				           (take 500)
-																				           (pages/report))
+																				           (pages/report)))
   (GET "/Quarterly/:yearmonth" [yearmonth]  (->> (data/getQuarterly yearmonth)
 																			           (take 500)
 																			           (map mapping/quarterlyToView)
-																			           (pages/report))
+																			           (pages/report)))
   (route/not-found "Not Found"))
 
 (def app
